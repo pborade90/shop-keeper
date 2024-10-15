4723009b219f56ffb3db5410e9c1268969dc64c3
Prathmesh Borade <125908123+pborade90@users.noreply.github.com>
2024-10-15 20:45:38 +0530

    changes in vite configuration file

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,16 @@
-import {defineConfig} from 'vite'
-
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: []	
-})
+	build: {
+		rollupOptions: {
+			output: {
+				assetFileNames: (assetInfo) => {
+					if (/\.png$/.test(assetInfo.name)) {
+						return 'assets/icons/[name][extname]'; // Avoid hash in filenames for icons
+					}
+					return 'assets/[name]-[hash][extname]'; // Keep hash for other files
+				},
+			},
+		},
+	},
+});
